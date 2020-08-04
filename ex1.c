9e74abb425f8726fe1f8f4545137d58b8c69c786
qzy93316 <49563995+qzy93316@users.noreply.github.com>
2020-08-04 16:10:54 +0800

    update some c lesson demo

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("---1---");
+    puts("---2---");
+    puts("---3---");
+    puts("---4---");
+    puts("---5---");
+    return 0;
+}
